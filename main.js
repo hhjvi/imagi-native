54e12a8d5a8b906a73d59227c9a310925bf95b8f
Pisces000221 <1786762946@qq.com>
2015-03-28 21:06:13 +0800

    简易HTML模板

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 (function () {
   'use strict';
-  var storage = {passports: [], entries: []};
+  window.storage = {passports: [], entries: []};
+  window.templates = [];
   function init_storage() {
     // Retrieve all data.
     var xhr = new XMLHttpRequest();
@@ -12,16 +13,53 @@
       var words = lines[i].split(' '), date = words[0].split('-'), operation = words[1];
       var date_str = date[0] + '-' + date[1] + '-' + date[2] + ' ' + date[3] + ':' + date[4] + ':' + date[5];
       if (operation === 'newcomer') {
-        storage.passports.push({name: words[2], passport: words.slice(3).join(' ')});
+        window.storage.passports.push({name: words[2], passport: words.slice(3).join(' ')});
       } else if (operation === 'entry') {
-        storage.entries.push({title: words[3], author: parseInt(words[2]), comments: []});
+        window.storage.entries.push({title: words[3], author: parseInt(words[2]), comments: []});
       } else if (operation === 'retitle') {
-        storage.entries[parseInt(words[3])].title = words.slice(4).join(' ');
+        window.storage.entries[parseInt(words[3])].title = words.slice(4).join(' ');
       } else if (operation === 'comment') {
-        storage.entries[parseInt(words[3])].comments.push({author: parseInt(words[2]), text: words.slice(4).join(' ')});
+        window.storage.entries[parseInt(words[3])].comments.push({author: parseInt(words[2]), text: words.slice(4).join(' ')});
       }
     }
-    console.log(storage);
   }
+
+  function init_templates() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'file:///home/lsq/develop/hhjvi/imagi-native/templates.tpl', false);
+    xhr.send();
+    // Split the templates.
+    var tpls = xhr.responseText.split('QwQ\n');
+    for (var i = 0; i < tpls.length; i++) {
+      var content_pos = tpls[i].indexOf('\n');
+      window.templates[tpls[i].substr(0, content_pos)] = tpls[i].substr(content_pos + 1);
+    }
+  }
+
   init_storage();
+  init_templates();
+
+  // http://segmentfault.com/blog/news/1190000000394948
+  // Call with ('aaa(% for (i = 0; i < this.a.length; i++) { %)(% this.a[i] %)x(% } %) nn', {a: [4, 'aa', 6]})
+  // Should be 'aaa4xaax6x nn'
+  function templater(html, options) {
+    var re = /\(%([^%>]+)?%\)/g, regex = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0, match;
+    function add(line, js) {
+      js ? (code += line.match(regex) ? line + '\n' : 'r.push(' + line + ');\n') :
+        (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+      return add;
+    };
+    while (match = re.exec(html)) {
+      add(html.slice(cursor, match.index))(match[1], true);
+      cursor = match.index + match[0].length;
+    }
+    add(html.substr(cursor, html.length - cursor));
+    code += 'return r.join("");';
+    return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
+  }
+  function render_template(name) {
+    document.getElementById('template-area').innerHTML = templater(window.templates[name]);
+  }
+
+  render_template('LOGIN');
 }());
---
+++ templates.tpl
@@ -0,0 +1,33 @@
+LOGIN
+<div class='content-title'>Log In</div>
+<div class='contents'>
+  <input id='login-passport' type='text' placeholder='Your passport'>
+  <button id='login-button' class='pure-button in-button'>Start</button>
+  <br><br>第一次来？直接输入一些字母或者数字作为登录通行证。登录后将会进行更详细的解释。
+</div>
+
+<style>
+  #login-passport {
+    margin-top: 2em;
+    font-family: 'Raleway';
+    font-size: 36px;
+    width: 100%;
+    border: 0px;
+    border-bottom: 1px solid #ccc;
+    padding-bottom: 1px;
+  }
+
+  #login-passport:focus {
+    border-bottom: 2px solid #fc9;
+    padding-bottom: 0px;
+  }
+
+
+  #login-button {
+    margin-top: 1em;
+    width: 100%;
+  }
+</style>
+
+QwQ
+LISTPAGE
