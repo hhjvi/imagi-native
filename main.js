a668f2c5919b26f80f55bef951ad3af869394a83
Pisces000221 <1786762946@qq.com>
2015-03-29 17:23:53 +0800

    改进标签显示

--- main.js
+++ main.js
@@ -81,7 +81,7 @@
     for (var i in tag) sum += tag.charCodeAt(i) * (i + 3);
     var ran1 = (tag.length * 103748 + sum * 33) % 256, ran2 = (tag.length * 6 + tag.charCodeAt(0) * 99 - sum) % 255, ran3 = Math.floor(tag.length / 1.18 - tag.charCodeAt(tag.length - 1) * 7.1 + sum * 99.122) % 256;
     var colour = '#' + hex_2dig(ran1) + hex_2dig(ran2) + hex_2dig(ran3);
-    return "<span class='entry-tag' style='background: " + colour + "'>" + tag + "</span>";
+    return "<span class='entry-tag" + (0.213 * ran1 + 0.715 * ran2 + 0.072 * ran3 > 127 ? "" : " inverse") + "' style='background: " + colour + "'>" + tag + "</span>";
   };
 
   // http://segmentfault.com/blog/news/1190000000394948
